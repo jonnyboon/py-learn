74c97ae1ad23693000ceeef4a3a80ac52d8f5e24
jonnyzar <64309336+jonnyzar@users.noreply.github.com>
2020-10-30 23:28:49 +0100

    recon

---
+++ recon_bot_server.py
@@ -0,0 +1,40 @@
+'''TCP Server with low level programming'''
+
+
+import socket, platform, os 
+
+
+def main():
+    SRV_ADDR = ""
+    SRV_PORT =  7777
+
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind((SRV_ADDR,SRV_PORT))
+        sock.listen(1)
+        conn, addr = sock.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                try:
+                    data = conn.recv(1024)
+                except:continue
+
+                print('received:', data.decode('utf-8').strip())      
+
+                if data.decode('utf-8').strip() == '1':
+                    snd_data = platform.platform() + " " + platform.machine()  
+                    conn.sendall(snd_data.encode())  
+                elif data.decode('utf-8').strip() == '0':
+                    conn.close()
+                    exit()
+                else:
+                    warn = "wrong selection"    
+                    conn.sendall(warn.encode())
+
+         
+
+
+if __name__ == "__main__":
+    main()
+
